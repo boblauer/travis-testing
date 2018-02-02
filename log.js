8a62ec21db5dc49dd868a9d6387e6ef2d74be693
Bob Lauer <rlauer@gmail.com>
2018-02-02 13:53:54 -0600

    Parse output

--- log.js
+++ log.js
@@ -1,1 +1,9 @@
-console.log('logg!!!!!');
+const fs = require('fs');
+
+const fileContents = fs.readFileSync('./output.txt', 'utf8');
+
+const passed = /(\d+)\spassing/.exec(fileContents);
+const failed = /(\d+)\sfailing/.exec(fileContents);
+
+console.log(passed ? passed[1] : 0);
+console.log(failed ? failed[1] : 0);
