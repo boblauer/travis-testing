83072aa79c80c2b5dbca0fde713cb3b16efb5fff
Bob Lauer <rlauer@gmail.com>
2018-02-02 14:07:29 -0600

    keep color, better log output

--- log.js
+++ log.js
@@ -5,5 +5,6 @@ const fileContents = fs.readFileSync('./output.txt', 'utf8');
 const passed = /(\d+)\spassing/.exec(fileContents);
 const failed = /(\d+)\sfailing/.exec(fileContents);
 
-console.log(passed ? passed[1] : 0);
-console.log(failed ? failed[1] : 0);
+console.log('Custom script being here:');
+console.log('passed:', passed ? passed[1] : 0);
+console.log('failed:', failed ? failed[1] : 0);
